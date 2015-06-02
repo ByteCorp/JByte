aa9b79ad8c9cfbfe68a82c3af0141775a7096d26
TomByte <tom@bytecorp.co.uk>
2015-06-02 22:34:14 +0100

    Increased variable limit to 10. Updated replace functionality.

--- JByte.js
+++ JByte.js
@@ -8,9 +8,16 @@
  * @param variable3
  * @returns {XML|string|*}
  * @constructor
- */
+ * @param variable4
+ * @param variable5
+ * @param variable6
+ * @param variable7
+ * @param variable8
+ * @param variable9
+ * @param variable10
+ * */
 
-function JByte(textToFormat, variable1, variable2, variable3, variable4, variable5, variable6) {
+function JByte(textToFormat, variable1, variable2, variable3, variable4, variable5, variable6, variable7, variable8, variable9, variable10) {
     var formattedText;
 
     variable1 = variable1 || "";
@@ -19,14 +26,22 @@ function JByte(textToFormat, variable1, variable2, variable3, variable4, variabl
     variable4 = variable4 || "";
     variable5 = variable5 || "";
     variable6 = variable6 || "";
+    variable7 = variable7 || "";
+    variable8 = variable8 || "";
+    variable9 = variable9 || "";
+    variable10 = variable10 || "";
 
     formattedText = textToFormat
-        .replace("{0}", variable1)
-        .replace("{1}", variable2)
-        .replace("{2}", variable3)
-        .replace("{3}", variable4)
-        .replace("{4}", variable5)
-        .replace("{5}", variable6);
+        .replace(new RegExp("(\\{0\\})", "g"), variable1)
+        .replace(new RegExp("(\\{1\\})", 'g'), variable2)
+        .replace(new RegExp("(\\{2\\})", 'g'), variable3)
+        .replace(new RegExp("(\\{3\\})", 'g'), variable4)
+        .replace(new RegExp("(\\{4\\})", 'g'), variable5)
+        .replace(new RegExp("(\\{5\\})", 'g'), variable6)
+        .replace(new RegExp("(\\{6\\})", 'g'), variable7)
+        .replace(new RegExp("(\\{7\\})", 'g'), variable8)
+        .replace(new RegExp("(\\{8\\})", 'g'), variable9)
+        .replace(new RegExp("(\\{9\\})", 'g'), variable10);
 
     return formattedText;
 }
